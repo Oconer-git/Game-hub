bea07d39859c09557cb4fd00c060d1d35fb99d09
Donell Oconer <donellpie@gmail.com>
2024-11-05 15:49:03 +0800

    initialize, added chakra ui

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+
+function App() {
+
+  return (
+    <div>
+      <Button colorScheme='blue'>Button</Button>
+    </div>
+  )
+}
+
+export default App
