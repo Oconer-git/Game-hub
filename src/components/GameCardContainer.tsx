55d96d30d8acceaa0bef35463b62e73d18138caf
Donell Oconer <donellpie@gmail.com>
2024-11-06 11:43:44 +0800

    fetch the genres

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -6,7 +6,7 @@ interface Props{
 }
 const GameCardContainer = ({children}: Props) => {
   return (
-    <Box width="200px" borderRadius={10} overflow="hidden" bg="coral">
+    <Box width="200px" borderRadius={10} overflow="hidden">
         {children}
     </Box>
   )
