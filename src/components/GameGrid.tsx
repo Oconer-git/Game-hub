198cdb2cedea7f22a30b4c02e0601d91d07fa9f9
Donell Oconer <donellpie@gmail.com>
2024-11-06 09:36:01 +0800

    display platform icons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -7,8 +7,7 @@ const GameGrid = () => {
     return (
         <>
             {error && <Text>{error}</Text>} 
-            <SimpleGrid columns={{sm: 1, md: 2, lg: 4, xl:5 }} padding='10' spacing={10}>
-            {/* <li key={game.id}>{game.name}</li> */}
+            <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl:4 }} padding='10' spacing={10}>
                 {games.map(game => <GameCard key={game.id} game={game}/>)}
             </SimpleGrid>
         </>
